165b944d2f8d4f8af52b8a5c368c8cdc0e9445e1
glody007 <glodymbutwile@gmail.com>
2021-06-16 01:20:23 -0700

    replace editorjs by quilljs

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -1,86 +1,93 @@
 <template>
   <div id="main">
-    <div id="editor">
-    </div>
+    <div ref="quill"></div>
   </div>
 </template>
 
 <script>
 
-import EditorJS from '@editorjs/editorjs'
-import Header from '@editorjs/header'
-import Paragraph from 'editorjs-paragraph-with-alignment'
-import ImageTool from '@editorjs/image'
-import ImageKit from "imagekit-javascript"
+import ImageKit from 'imagekit-javascript'
+import Quill from 'quill'
 
 let imagekit = new ImageKit({
-    urlEndpoint: "https://ik.imagekit.io/tlr7lkiwqbj",
-    publicKey: "public_7BOsJo+gJzQeU+O7WgThIpcpIc4=",
-    authenticationEndpoint: "api/auth/uploadendpoint"
-})
-
-let upload = (file) => {
-  return new Promise((resolve, reject) => {
-      imagekit.upload({
-          file: file,
-          fileName: "abc1.jpg",
-          tags: ["tag1"]
-      }, function(err, result) {
-            if (result) resolve(result.url)
-            else reject(new Error('upload failed'))
-      })
-    })
-    .then(url => {
-      return {
-                success: 1,
-                file: { url: url }
-              }
-    })
-    .catch(err => {
-      console.log(err)
-      return { success: 0 }
-    })
-}
-
-const editor = new EditorJS({
-  holder: 'editor',
-  tools: {
-    paragraph: {
-      placeholder: 'Entrer un titre',
-      class: Paragraph,
-      inlineToolbar: true
-    },
-    header: {
-      class: Header,
-      config: {
-        placeholder: 'Enter a header',
-        levels: [1, 2, 3, 4],
-        defaultLevel: 3
-      }
-    },
-    image: {
-      class: ImageTool,
-      config: {
-        uploader: {
-          uploadByFile (file) {
-            return upload(file)
-          },
-          uploadByUrl (url) {
-            return upload(url)
-          }
-        }
-      }
-    }
-  }
+  urlEndpoint: 'https://ik.imagekit.io/tlr7lkiwqbj',
+  publicKey: 'public_7BOsJo+gJzQeU+O7WgThIpcpIc4=',
+  authenticationEndpoint: 'api/auth/uploadendpoint'
 })
 
 export default {
   name: 'editor',
+  data () {
+    return {
+      content: {
+        ops: []
+      },
+      editor: null,
+      config: {
+        modules: {
+          toolbar: [
+            ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+            ['blockquote', 'code-block', 'image'],
+
+            [{ 'header': 1 }, { 'header': 2 }], // custom button values
+            [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+            [{ 'script': 'sub' }, { 'script': 'super' }], // superscript/subscript
+            [{ 'indent': '-1' }, { 'indent': '+1' }], // outdent/indent
+            [{ 'direction': 'rtl' }],
+
+            [{ 'size': ['small', false, 'large', 'huge'] }], // custom dropdown
+            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+            [{ 'color': [] }, { 'background': [] }], // dropdown with defaults from theme
+            [{ 'font': [] }],
+            [{ 'align': [] }],
+
+            ['clean'] // remove formatting button
+          ]
+        },
+        readOnly: false,
+        placeholder: 'Ecrivez ici...',
+        theme: 'snow'
+      }
+    }
+  },
   components: {
 
   },
+  mounted () {
+    this.editor = new Quill(this.$refs.quill, this.config)
+    this.editor.getModule('toolbar').addHandler('image', this.selectLocalImage)
+  },
   methods: {
+    saveToServer (file) {
+      imagekit.upload({
+        file: file,
+        fileName: 'file.jpg'
+      }, this.uploadCallback)
+    },
+    selectLocalImage () {
+      const input = document.createElement('input')
+      input.setAttribute('type', 'file')
+      input.click()
 
+      // Listen upload local image and save to server
+      input.onchange = () => {
+        const file = input.files[0]
+
+        // file type is only image.
+        if (/^image\//.test(file.type)) {
+          this.saveToServer(file)
+        } else {
+          console.warn('You could only upload images.')
+        }
+      }
+    },
+    uploadCallback (err, result) {
+      if (result) {
+        const range = this.editor.getSelection()
+        this.editor.insertEmbed(range.index, 'image', result.url)
+      } else console.log(err)
+    }
   }
 }
 </script>
