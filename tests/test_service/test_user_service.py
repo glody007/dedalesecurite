d96448ad72ce6d813c2c5b52261ead86eb663e85
glody007 <glodymbutwile@gmail.com>
2021-06-17 05:22:53 -0700

    complete test for user service

--- tests/test_service/test_user_service.py
+++ tests/test_service/test_user_service.py
@@ -1,6 +1,7 @@
 import pytest
 from app.services.user import *
 from app.models.user import UserType
+from . import raiseError
 from .. import (drop_all,
                 exemple_user_data)
 
@@ -16,6 +17,38 @@ def test_add(drop_all, exemple_user_data):
     assert UserService.count() == 0
     UserService.add(user)
     assert UserService.count() == 1
+
+def test_insert(drop_all, exemple_user_data):
+    assert UserService.count() == 0
+    UserService.insert(exemple_user_data)
+    assert UserService.count() == 1
+
+def test_delete_or(drop_all, exemple_user_data):
+    user = UserService.insert(exemple_user_data)
+    assert UserService.count() == 1
+    with pytest.raises(ValueError):
+        UserService.delete_or('55153a8014829a865bbf700d', raiseError)
+    UserService.delete_or(user.id, raiseError)
+    assert UserService.count() == 0
+
+def test_get_or(drop_all, exemple_user_data):
+    user = UserService.insert(exemple_user_data)
+    with pytest.raises(ValueError):
+        UserService.get_or('55153a8014829a865bbf700d', raiseError)
+    UserService.get_or(user.id, raiseError)
+
+def test_update_or(drop_all, exemple_user_data):
+    user = UserService.insert(exemple_user_data)
+    data = {'nom':'root', 'phone_number':'9999966666', 'email':'rot@root.com'}
+    with pytest.raises(ValueError):
+        UserService.update_or('55153a8014829a865bbf700d', data, raiseError)
+
+    UserService.update_or(user.id, data, raiseError)
+    user = UserService.get(user.id)
+    assert user.nom == data['nom']
+    assert user.email == data['email']
+    assert user.type == UserType.ENREGISTRER
+    assert user.phone_number ==  data['phone_number']
 
 def test_register(drop_all, exemple_user_data):
     assert UserService.count() == 0
